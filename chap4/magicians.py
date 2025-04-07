54133c3469e0bf45237d788d49bbb93e5bd4fe46
Nestor Ramirez <nramirez@pilotpointtx.gov>
2025-04-07 12:36:15 -0500

    Chap 4 for loop

---
+++ chap4/magicians.py
@@ -0,0 +1,7 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    #print(magician)
+
+    #Doing more within a for loop
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your nex trick, {magician.title()}.\n")
